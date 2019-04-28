87839d2e06983e048fa802ffb0acfe0e927ce3f1
Grischa Aranda <grischa.aranda@core-it.ch>
2019-04-28 21:52:05 +0100

    Chore: Remove old programmes & add vscode

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -3,15 +3,12 @@ brew tap caskroom/cask
 
 # Install packages
 apps=(
-    atom
-    dropbox
     iterm2
-    macdown
     slack
-    sublime-text
     vagrant
     vagrant-manager
     virtualbox
+    visual-studio-code
 )
 
 brew cask install "${apps[@]}"
